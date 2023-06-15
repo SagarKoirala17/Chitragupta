e94bd3751b7626eed79efd21310ff6fa832d76a2
Sagar Koirala <koiralasagar138@gmail.com>
2023-06-15 10:41:30 +0545

    generate the unique email each time

---
+++ cypress/e2e/unique_email.js
@@ -0,0 +1,7 @@
+ export function generateUniqueEmail() {
+    var timestamp = Date.now();
+    var randomString = Math.random().toString(36).substring(2, 10);
+    var email = "unique" + timestamp + "_" + randomString + "@danpheit.com";
+    return email;
+  }
+  
